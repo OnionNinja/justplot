c4947f253cc8db892e716269afcd7e6918feb7d4
U <cayoglu@me.com>
2016-01-21 14:21:13 +0100

    added dependencies

--- setup.py
+++ setup.py
@@ -5,12 +5,16 @@ from distutils.core import setup
 
 setup(
     name='justplot',
-    version='0.1.2a1',
+    version='0.1.2a3',
     packages=['justplot',],
     license='MIT',
     author='Uğur Çayoğlu',
     author_email='urcyglu@gmail.com',
     url='https://github.com/OnionNinja/justplot',
+    install_requires=[
+        'matplotlib',
+        'numpy',
+    ],
     classifiers=[
         # More at http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 2 - Pre-Alpha',
