ff79c4ffac8b6cfb2cca42aacd8086dea3ad8dfd
U <cayoglu@me.com>
2016-01-21 20:47:43 +0100

    setup update for inclusion of README.md

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ from distutils.core import setup
 
 setup(
     name='justplot',
-    version='0.1.2a3',
+    version='0.1.2a5',
     packages=['justplot',],
     license='MIT',
     author='Uğur Çayoğlu',
     author_email='urcyglu@gmail.com',
     description='A thin wrapper around matplotlib, which lets you reuse settings.',
     url='https://github.com/OnionNinja/justplot',
+    download_url='https://github.com/onionninja/justplot/tarball/v0.1.2',
     install_requires=[
         'matplotlib',
         'numpy',
@@ -24,6 +25,7 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3 :: Only',
       ],
-    keywords=['plots','matplotlib','wrapper']
+    keywords=['plots','matplotlib','wrapper'],
+    include_package_data=True,
 )
 
